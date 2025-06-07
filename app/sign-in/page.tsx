eddd5c7589b1cb0cc006326a1d92f8f2863bb20b
mazed <ablmazed@gmail.com>
2025-06-08 00:39:55 +0600

    commit 14

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import SignIn from '@/components/shared/sign-in'
+
+export default function page() {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  )
+}
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -2,3 +2,8 @@ export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Amazona Modern'
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   'An Amazon clone built with Next.js, Postgres, Shgfdgdfadcn'
+
+export const signInDefaultValues = {
+  email: '',
+  password: '',
+}
