db0d61103371b1e5a7c5d56249b1e45623edc70e
mazed <ablmazed@gmail.com>
2025-06-08 08:08:55 +0600

    commit 16

--- app/(auth)/sign-in/layout.tsx
+++ app/(auth)/sign-in/layout.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+// import React from 'react'
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="flex-center min-h-screen w-full ">{children}</div>
-}
+// const Layout = ({ children }: { children: React.ReactNode }) => {
+//   return <div className="flex-center min-h-screen w-full ">{children}</div>
+// }
 
-export default Layout
+// export default Layout
