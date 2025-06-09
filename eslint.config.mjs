2cdee12f570ccf720ee63109024a81932a919e3f
mazed <ablmazed@gmail.com>
2025-06-09 10:06:26 +0600

    commit 27

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,13 +9,17 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
+// const eslintConfig = [
+//   ...compat.extends('next/core-web-vitals', 'next/typescript'),
+// ]
+
 const eslintConfig = [
   ...compat.config({
     extends: ['next'],
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
-      '@any': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   }),
 ]
