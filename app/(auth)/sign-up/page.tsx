935714d754060282cc65758e193b474a7ed31c03
mazed <ablmazed@gmail.com>
2025-06-09 11:06:09 +0600

    commit 37

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,57 +1,13 @@
-import { Metadata } from 'next'
-import Image from 'next/image'
-import Link from 'next/link'
+// app/signin/page.tsx (or wherever your SignIn page is)
+
 import { redirect } from 'next/navigation'
-
 import { auth } from '@/auth'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
-import { APP_NAME } from '@/lib/constants'
+import SignUpClient from './sign-up-client'
 
-import SignUpForm from './signup-form'
-
-export const metadata: Metadata = {
-  title: `Sign Up - ${APP_NAME}`,
-}
-
-export default async function SignUp({
-  searchParams: { callbackUrl },
-}: {
-  searchParams: {
-    callbackUrl: string
-  }
-}) {
+export default async function SignInPage() {
   const session = await auth()
   if (session) {
-    return redirect(callbackUrl || '/')
+    return redirect('/')
   }
 
-  return (
-    <div className="w-full max-w-md mx-auto">
-      <Card>
-        <CardHeader className="space-y-4">
-          <Link href="/" className="flex-center">
-            <Image
-              src="/assets/icons/logo.svg"
-              width={100}
-              height={100}
-              alt={`${APP_NAME} logo`}
-            />
-          </Link>
-          <CardTitle className="text-center">Create Account</CardTitle>
-          <CardDescription className="text-center">
-            Enter your information below to create your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <SignUpForm />
-        </CardContent>
-      </Card>
-    </div>
-  )
-}
+  return <SignUpClient />
