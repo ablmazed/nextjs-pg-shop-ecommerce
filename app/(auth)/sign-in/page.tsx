b0a69449c9b613839d7a218ca8bade973256a28d
mazed <ablmazed@gmail.com>
2025-06-09 10:56:06 +0600

    commit 36

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,58 +1,14 @@
-import { Metadata } from 'next'
-import Image from 'next/image'
-import Link from 'next/link'
+// app/signin/page.tsx (or wherever your SignIn page is)
+
 import { redirect } from 'next/navigation'
-
 import { auth } from '@/auth'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
-import { APP_NAME } from '@/lib/constants'
+import SignInClient from './sign-in-client'
 
-import CredentialsSignInForm from './credentials-signin-form'
-
-export const metadata: Metadata = {
-  title: `Sign In - ${APP_NAME}`,
-}
-
-interface PageProps {
-  searchParams: {
-    callbackUrl?: string
-  }
-}
-export default async function SignIn({ searchParams }: PageProps) {
-  const { callbackUrl } = searchParams
-
+export default async function SignInPage() {
   const session = await auth()
   if (session) {
-    return redirect(callbackUrl || '/')
+    return redirect('/')
   }
 
-  return (
-    <div className="w-full max-w-md mx-auto">
-      <Card>
-        <CardHeader className="space-y-4">
-          <Link href="/" className="flex-center">
-            <Image
-              src="/assets/icons/logo.svg"
-              width={100}
-              height={100}
-              alt={`${APP_NAME} logo`}
-            />
-          </Link>
-          <CardTitle className="text-center">Sign In</CardTitle>
-          <CardDescription className="text-center">
-            Select a method to sign in to your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="space-y-4">
-          <CredentialsSignInForm />
-        </CardContent>
-      </Card>
-    </div>
-  )
+  return <SignInClient />
 }
