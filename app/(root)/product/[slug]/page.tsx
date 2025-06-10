3569ba2855295ed7b4cfe3c46099ca3d3150b76a
mazed <ablmazed@gmail.com>
2025-06-10 13:47:24 +0600

    commit 25

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -6,6 +6,10 @@ import { Card, CardContent } from '@/components/ui/card'
 import { getProductBySlug } from '@/lib/actions/product.actions'
 import { APP_NAME } from '@/lib/constants'
 import { Button } from '@/components/ui/button'
+import AddToCart from '@/components/shared/product/add-to-cart'
+import { round2 } from '@/lib/utils'
+import { getMyCart } from '@/lib/actions/cart.actions'
+import { auth } from '@/auth'
 
 type Props = {
   params: Promise<{
@@ -35,6 +39,9 @@ const ProductDetails = async ({ params }: Props) => {
 
   const product = await getProductBySlug(slug)
   if (!product) notFound()
+
+  const cart = await getMyCart()
+  const session = await auth()
 
   return (
     <>
@@ -88,7 +95,17 @@ const ProductDetails = async ({ params }: Props) => {
                 </div>
                 {product.stock !== 0 && (
                   <div className=" flex-center">
-                    <Button className="w-full">Add to cart</Button>
+                    <AddToCart
+                      cart={cart}
+                      item={{
+                        productId: product.id,
+                        name: product.name,
+                        slug: product.slug,
+                        price: round2(product.price),
+                        qty: 1,
+                        image: product.images![0],
+                      }}
+                    />
                   </div>
                 )}
               </CardContent>
---
+++ components/shared/product/add-to-cart.tsx
@@ -0,0 +1,97 @@
+import { Button } from '@/components/ui/button'
+import { removeItemFromCart, addItemToCart } from '@/lib/actions/cart.actions'
+import { round2 } from '@/lib/utils'
+import { Cart, CartItem } from '@/types'
+import { Loader, Minus, Plus } from 'lucide-react'
+import { useRouter } from 'next/router'
+import { useTransition } from 'react'
+import { toast } from 'sonner'
+
+export default function AddToCart({
+  cart,
+  item,
+}: {
+  cart?: Cart
+  item: Omit<CartItem, 'cartId'>
+}) {
+  const router = useRouter()
+  const [isPending, startTransition] = useTransition()
+  const existItem =
+    cart && cart.items.find((x) => x.productId === item.productId)
+  return existItem ? (
+    <div>
+      <Button
+        type="button"
+        variant="outline"
+        disabled={isPending}
+        onClick={() => {
+          startTransition(async () => {
+            const res = await removeItemFromCart(item.productId)
+            toast(
+              res.success
+                ? toast.success(res.message)
+                : toast.error(res.message)
+            )
+            return
+          })
+        }}
+      >
+        {isPending ? (
+          <Loader className="w-4 h-4  animate-spin" />
+        ) : (
+          <Minus className="w-4 h-4" />
+        )}
+      </Button>
+      <span className="px-2">{existItem.qty}</span>
+      <Button
+        type="button"
+        variant="outline"
+        disabled={isPending}
+        onClick={() => {
+          startTransition(async () => {
+            const res = await addItemToCart({
+              ...item,
+              price: item.price,
+            })
+            toast(toast.success(res.message))
+            return
+          })
+        }}
+      >
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Plus className="w-4 h-4" />
+        )}
+      </Button>
+    </div>
+  ) : (
+    <Button
+      className="w-full"
+      type="button"
+      disabled={isPending}
+      onClick={() => {
+        startTransition(async () => {
+          const res = await addItemToCart({
+            ...item,
+            price: round2(item.price),
+          })
+          if (!res.success) {
+            toast(toast.error(res.message))
+            return
+          }
+          toast(' Event has been', {
+            description: `${item.name} added to the cart`,
+            action: {
+              label: 'Go to cart',
+              onClick: () => router.push('/cart'),
+            },
+          })
+        })
+      }}
+    >
+      {isPending ? <Loader className="animate-spin" /> : <Plus />}
+      Add to cart
+    </Button>
+  )
+}
