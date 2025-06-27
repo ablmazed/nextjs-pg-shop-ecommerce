c2bc5572bddabcbe611a98a64dd5e1c273f70652
mazed <ablmazed@gmail.com>
2025-06-27 22:34:59 +0600

    73 commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,14 @@
+import ProductCarousel from '@/components/shared/product/product-carousel'
 import ProductList from '@/components/shared/product/product-list'
-import { getLatestProducts } from '@/lib/actions/product.actions'
+import {
+  getFeaturedProducts,
+  getLatestProducts,
+} from '@/lib/actions/product.actions'
 import { APP_DESCRIPTION, APP_NAME } from '@/lib/constants'
 import { Metadata } from 'next'
+
+const latestProducts = await getLatestProducts()
+const featuredProducts = await getFeaturedProducts()
 
 export const metadata: Metadata = {
   title: `${APP_NAME} - ${APP_DESCRIPTION}`,
@@ -11,7 +18,14 @@ export default async function Home() {
   const latestProducts = await getLatestProducts()
   return (
     <div>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
+
       <ProductList title="Newest Arrivals" data={latestProducts} />
+      {/* <ProductList title="Newest Arrivals" data={latestProducts} page={1} /> */}
+      {/* <ProductPromotion />
+      <EcommerceFeatures /> */}
     </div>
   )
 }
