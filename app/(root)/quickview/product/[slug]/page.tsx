a4e00758d32610629efadbc3e7d68668459b9808
mazed <ablmazed@gmail.com>
2025-06-30 22:21:38 +0600

    92 commit

--- app/(root)/quickview/product/[slug]/page.tsx
+++ app/(root)/quickview/product/[slug]/page.tsx
@@ -1,5 +1,9 @@
 import { redirect } from 'next/navigation'
 
-export default async function QuickViewPage(params: Promise<{ slug: string }>) {
+type Props = {
+  params: Promise<{ slug: string }>
+}
+
+export default async function QuickViewPage({ params }: Props) {
   redirect(`/product/${(await params).slug}`)
 }
