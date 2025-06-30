c60a65762e88b42625554742923119fe39ccb523
mazed <ablmazed@gmail.com>
2025-06-30 14:32:58 +0600

    86 commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,13 +3,16 @@ import Header from '@/components/shared/header'
 
 export default function RootLayout({
   children,
+  modal,
 }: {
   children: React.ReactNode
+  modal: React.ReactNode
 }) {
   return (
     <div className="flex h-screen flex-col">
       <Header />
       <main className="flex-1 wrapper">{children}</main>
+      {modal}
       <Footer />
     </div>
   )
